63bdb6dc3cae76b953ed7c6c6935618f6eb071cf
Severino Vinicius <severino.sales95@gmail.com>
2023-05-02 21:47:04 -0300

    [Requisito 03] - Finalizado

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import pokemonData from '../data';
+// import App from '../App';
+
+describe('Teste o componente <FavoritePokemon.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos;', () => {
+    const pokemonList = [];
+    renderWithRouter(<FavoritePokemon pokemonList={ pokemonList } />);
+
+    const noFavorite = screen.getByText(/no favorite pokémon found/i);
+
+    expect(noFavorite).toBeInTheDocument();
+  });
+  test('Teste se apenas são exibidos os Pokémon favoritados', () => {
+    const pokemonList = [pokemonData[1]];
+    renderWithRouter(<FavoritePokemon pokemonList={ pokemonList } />);
+
+    const image = screen.getByRole('img', {
+      name: /is marked as favorite/i,
+    });
+
+    expect(image).toHaveAttribute('alt', image.alt);
+  });
+});
